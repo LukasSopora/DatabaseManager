2fddfed5707f05a41bc31d1d96962f8be724114c
Lukas Sopora <lukas@sopora.de>
2019-04-04 14:05:22 +0200

    implement class artist

--- DatabaseManager/Model/Artist.cs
+++ DatabaseManager/Model/Artist.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace DatabaseManager.Model
 {
-    class Artist
+    public class Artist
     {
+        private int m_Id;
+        private string m_Name;
+        private int m_Year;
+        private string m_Country;
+
+        public string Country
+        {
+            get { return m_Country; }
+            set { m_Country = value; }
+        }
+
+        public int Year
+        {
+            get { return m_Year; }
+            set { m_Year = value; }
+        }
+
+        public string Name
+        {
+            get { return m_Name; }
+            set { m_Name = value; }
+        }
+
+        public int Id
+        {
+            get { return m_Id; }
+            set { m_Id = value; }
+        }
     }
 }
