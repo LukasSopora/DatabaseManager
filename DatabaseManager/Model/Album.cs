5d353d0bbaab5a6f0658a013aece723d263bf098
Lukas Sopora <lukas@sopora.de>
2019-04-04 14:07:16 +0200

    implement Album class

--- DatabaseManager/Model/Album.cs
+++ DatabaseManager/Model/Album.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace DatabaseManager.Model
 {
-    class Album
+    public class Album
     {
+        private int m_Id;
+        private int m_Year;
+        private string m_Country;
+
+        public string Country
+        {
+            get { return m_Country; }
+            set { m_Country = value; }
+        }
+
+        public int Year
+        {
+            get { return m_Year; }
+            set { m_Year = value; }
+        }
+
+        public int Id
+        {
+            get { return m_Id; }
+            set { m_Id = value; }
+        }
     }
 }
