fe9a9c5febe85ffe4baf7b52b1e2961b2297842e
Lukas Sopora <lukas@sopora.de>
2019-04-08 00:37:52 +0200

    implement GetAllArtists function

--- DatabaseManager/Database/QueryHelper.cs
+++ DatabaseManager/Database/QueryHelper.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿using DatabaseManager.Model;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DatabaseManager.Database
 {
-    public class QueryHelper
+    public static class QueryHelper
     {
-        public static void InitDatabase()
+        public static IList<Artist> GetAllArtists()
         {
+            if(!File.Exists(DB_Constants.DB_Artist_Path))
+            {
+                return null;
+            }
+
+            var lines = File.ReadAllText(DB_Constants.DB_Artist_Path);
+            IList<Artist> result = JsonConvert.DeserializeObject<List<Artist>>(lines);
+
+            return result;
         }
     }
 }
